45f8e281b0397686ceddd41b3bb2ad5b4edda8dc
Nolan <poro2420033@gmail.com>
2025-03-01 09:50:32 +0700

    level14

--- Assets/_Scripts/Manager/Level Manager.cs
+++ Assets/_Scripts/Manager/Level Manager.cs
@@ -11,7 +11,7 @@ public class LevelManager : Singleton<LevelManager>
 
     void Awake()
     {
-        PlayerPrefs.SetInt("CurrentLevel", 13);
+        PlayerPrefs.SetInt("CurrentLevel", 14);
         currentLevelIndex = PlayerPrefs.GetInt("CurrentLevel", 1);
         LoadLevel(currentLevelIndex);
 
